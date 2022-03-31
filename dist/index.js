8316acb9fb338c0be7c80f281e68f7077247c92d
Connor Tumbleson <connor@sourcetoad.com>
2022-03-31 15:44:29 -0400

    SIP-253 feat: support callback for ssh auth

--- dist/index.js
+++ dist/index.js
@@ -64,9 +64,9 @@ const lifetimeInSeconds = core.getInput('lifetime_in_seconds');
 
 console.log(`Attempting to create ${socketPath}...`);
 
-function executeCommand(command) {
+function executeCommand(command, cb = null) {
     try {
-        execSync(command);
+        execSync(command, cb);
     } catch (e) {
         if (e.message.contains('Address already in use')) {
             core.info('Agent already exists on sock. Skipping creation.');
@@ -82,6 +82,14 @@ executeCommand(`sed -i -e '/^${host} /d' ~/.ssh/known_hosts`);
 executeCommand(`ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts`);
 executeCommand(`eval $(ssh-agent -a "${socketPath}")`);
 executeCommand(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
+
+executeCommand('echo $SSH_AGENT_PID', function (err, stdout) {
+    core.exportVariable('SSH_AGENT_PID', stdout);
+});
+
+executeCommand('echo $SSH_AUTH_SOCK', function (err, stdout) {
+    core.exportVariable('SSH_AUTH_SOCK', stdout);
+});
 
 core.setOutput('socket-path', socketPath);
 core.info('Done; exiting.');
