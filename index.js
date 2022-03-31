317c101f6c162e675fe4c375660cd63ef64fc57c
Connor Tumbleson <connor@sourcetoad.com>
2022-03-31 13:08:45 -0400

    SIP-253 feat: allow commands to run individually

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ const key = core.getInput('key');
 const lifetimeInSeconds = core.getInput('lifetime_in_seconds');
 
 console.log(`Attempting to create ${socketPath}...`);
-try {
-    execSync(
-        'mkdir -p ~/.ssh && ' +
-        'touch ~/.ssh/known_hosts && ' +
-        `sed -i -e '/^${host} /d' ~/.ssh/known_hosts && ` +
-        `ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts && ` +
-        `eval $(ssh-agent -a "${socketPath}") && ` +
-        `echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`
-    );
-} catch (e) {
-    console.error(e.message);
-    process.exit(1);
+
+function executeCommand(command) {
+    try {
+        execSync(command);
+    } catch (e) {
+        if (e.message.contains('Address already in use')) {
+            console.warn('Agent already exists on sock. Skipping creation.');
+        } else {
+            console.error(e.message);
+            process.exit(1);
+        }
+    }
 }
+
+executeCommand('mkdir -p ~/.ssh');
+executeCommand('touch ~/.ssh/known_hosts');
+executeCommand(`sed -i -e '/^${host} /d' ~/.ssh/known_hosts`);
+executeCommand(`ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts`);
+executeCommand(`eval $(ssh-agent -a "${socketPath}")`);
+executeCommand(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
 
 console.log(`Created ${socketPath}`);
 core.setOutput('socket-path', socketPath);
