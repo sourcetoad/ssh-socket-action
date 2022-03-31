a1a4aeb5117167bca0d2bd0477a9728107d2d678
Connor Tumbleson <connor@sourcetoad.com>
2022-03-31 17:16:59 -0400

    SIP-253 fix: output as string

--- dist/index.js
+++ dist/index.js
@@ -78,16 +78,20 @@ function executeCommand(command) {
     }
 }
 
+// Prepare the host file.
 executeCommand('mkdir -p ~/.ssh');
 executeCommand('touch ~/.ssh/known_hosts');
 executeCommand(`sed -i -e '/^${host} /d' ~/.ssh/known_hosts`);
 executeCommand(`ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts`);
-executeCommand(`ssh-agent -a "${socketPath}"`);
-executeCommand(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
 
+// Start the agent (or re-use one)
+executeCommand(`ssh-agent -a "${socketPath}"`);
 const pid = executeCommand(`lsof -Fp ${socketPath} | head -n 1 | sed 's/^p//'`)
-core.exportVariable('SSH_AGENT_PID', pid);
+core.exportVariable('SSH_AGENT_PID', parseInt(pid));
 core.exportVariable('SSH_AUTH_SOCK', socketPath);
+
+// Add the key
+executeCommand(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
 
 core.setOutput('socket-path', socketPath);
 core.info('Done; exiting.');
