19f8335c564ff85dbadccda5063795320bb86f3a
Connor Tumbleson <connor@sourcetoad.com>
2022-03-31 17:19:14 -0400

    SIP-253 fix: output as string

--- dist/index.js
+++ dist/index.js
@@ -64,7 +64,7 @@ const lifetimeInSeconds = core.getInput('lifetime_in_seconds');
 
 console.log(`Attempting to create ${socketPath}...`);
 
-function executeCommand(command) {
+function execSyncAndReturn(command) {
     try {
         return execSync(command, {
             encoding: 'utf-8'
@@ -79,19 +79,19 @@ function executeCommand(command) {
 }
 
 // Prepare the host file.
-executeCommand('mkdir -p ~/.ssh');
-executeCommand('touch ~/.ssh/known_hosts');
-executeCommand(`sed -i -e '/^${host} /d' ~/.ssh/known_hosts`);
-executeCommand(`ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts`);
+execSync('mkdir -p ~/.ssh');
+execSync('touch ~/.ssh/known_hosts');
+execSync(`sed -i -e '/^${host} /d' ~/.ssh/known_hosts`);
+execSync(`ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts`);
 
 // Start the agent (or re-use one)
-executeCommand(`ssh-agent -a "${socketPath}"`);
-const pid = executeCommand(`lsof -Fp ${socketPath} | head -n 1 | sed 's/^p//'`)
+execSync(`ssh-agent -a "${socketPath}"`);
+const pid = execSyncAndReturn(`lsof -Fp ${socketPath} | head -n 1 | sed 's/^p//'`)
 core.exportVariable('SSH_AGENT_PID', parseInt(pid));
 core.exportVariable('SSH_AUTH_SOCK', socketPath);
 
 // Add the key
-executeCommand(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
+execSync(`echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`);
 
 core.setOutput('socket-path', socketPath);
 core.info('Done; exiting.');
