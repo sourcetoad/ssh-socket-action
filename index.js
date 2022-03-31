8450dd58fffa42c769fd45d57a6d43cb8a2a5421
Connor Tumbleson <connor@sourcetoad.com>
2022-03-31 12:07:29 -0400

    SIP-253 fix: ensure known host is cleaned + lifetime

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const host = core.getInput('host');
 const port = core.getInput('port');
 const socketPath = core.getInput('socket-path');
 const key = core.getInput('key');
+const lifetimeInSeconds = core.getInput('lifetime_in_seconds');
 
 console.log(`Attempting to create ${socketPath}...`);
 try {
     execSync(
         'mkdir -p ~/.ssh && ' +
+        'touch ~/.ssh/known_hosts && ' +
+        `sed -i -e '/^${host} /d' ~/.ssh/known_hosts`
         `ssh-keyscan${port ? ` -p ${port}` : ''} "${host}" >> ~/.ssh/known_hosts && ` +
         `eval $(ssh-agent -a "${socketPath}") && ` +
-        `echo "${key}" | base64 -d | ssh-add -`
+        `echo "${key}" | base64 -d | ssh-add -t ${lifetimeInSeconds} -`
     );
 } catch (e) {
     console.error(e.message);
